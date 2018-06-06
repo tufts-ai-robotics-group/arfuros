4d218d07e5816468e1b0f5d7ca9439797d33d780
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-06-06 19:45:37 -0400

    Directory Reorganization

---
+++ Source/Unity/ARFUROS/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour {
+
+	public void updateIP(string newstring)
+    {
+        PlayerPrefs.SetString("IP", newstring);
+    }
+
+    public void updateLaserScan(bool newbool)
+    {
+        int val = newbool ? 1 : 0;
+        PlayerPrefs.SetInt("LaserScan", val);
+    }
+
+    public void updatePath(bool newbool)
+    {
+        int val = newbool ? 1 : 0;
+        PlayerPrefs.SetInt("Path", val);
+    }
+
+    public void onApply()
+    {
+        SceneManager.LoadScene("arview");
+    }
+
+}
+
