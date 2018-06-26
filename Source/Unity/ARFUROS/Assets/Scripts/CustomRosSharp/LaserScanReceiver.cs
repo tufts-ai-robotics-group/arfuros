943ff90671af8c803f91110a14137d1842c615f5
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-06-26 14:48:59 -0400

    Fixed Ros Sharp

--- Source/Unity/ARFUROS/Assets/Scripts/CustomRosSharp/LaserScanReceiver.cs
+++ Source/Unity/ARFUROS/Assets/Scripts/CustomRosSharp/LaserScanReceiver.cs
@@ -5,8 +5,8 @@ namespace RosSharp.RosBridgeClient
 {
     public class LaserScanReceiver : MessageReceiver
     {
-        public override Type MessageType { get { return (typeof(StandardString)); } }
-        private StandardString rawMessage;
+        public override Type MessageType { get { return (typeof(SensorLaserScan)); } }
+        private SensorLaserScan rawMessage;
         
         public float angle_min;
         public float angle_max;
@@ -23,36 +23,19 @@ namespace RosSharp.RosBridgeClient
 
         private void ReceiveMessage(object sender, MessageEventArgs e)
         {
-            rawMessage = (StandardString)e.Message;
-            convertToScan();
+            rawMessage = (SensorLaserScan)e.Message;
+            UpdateValues();
         }
 
-        private void convertToScan()
+        private void UpdateValues()
         {
-            char[] charSeparators = new char[] { '|' };
-            string[] result;
-
-            result = rawMessage.data.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
-
-            angle_min = Convert.ToSingle(result[0]);
-
-            angle_max = Convert.ToSingle(result[1]);
-
-            angle_increment = Convert.ToSingle(result[2]);
-
-            range_min = Convert.ToSingle(result[3]);
-            range_max = Convert.ToSingle(result[4]);
-            range_size = Convert.ToInt32(result[5]);
-
-            ranges = new float[range_size];
-            for (int i = 0; i < range_size; i++)
-            {
-                string curr = result[6 + i];
-                if (curr == "nan")
-                    ranges[i] = -1;
-                else
-                    ranges[i] = Convert.ToSingle(curr);
-            }
+            angle_min = rawMessage.angle_min;
+            angle_max = rawMessage.angle_max;
+            angle_increment = rawMessage.angle_increment;
+            range_min = rawMessage.range_min;
+            range_max = rawMessage.range_max;
+            range_size = rawMessage.ranges.Length;
+            ranges = rawMessage.ranges;
         }
     }
 }
