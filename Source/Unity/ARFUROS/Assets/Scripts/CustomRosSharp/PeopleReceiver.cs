92a52bd1e7e715b5cf3ea9c3282ec3cb7af06afe
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-06-28 20:20:38 -0400

    People Receiver Added

---
+++ Source/Unity/ARFUROS/Assets/Scripts/CustomRosSharp/PeopleReceiver.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PeopleReceiver : MessageReceiver
+    {
+        public override Type MessageType { get { return (typeof(PeoplePositionMeasurementArray)); } }
+        private PeoplePositionMeasurementArray rawMessage;
+        public Vector3[] people;
+
+
+        private void Awake()
+        {
+            MessageReception += ReceiveMessage;
+        }
+
+        private void ReceiveMessage(object sender, MessageEventArgs e)
+        {
+            rawMessage = (PeoplePositionMeasurementArray)e.Message;
+            UpdateValues();
+        }
+
+        private void UpdateValues() {
+
+            people = new Vector3 [rawMessage.people.Length];
+
+            for(int i = 0; i < rawMessage.people.Length; i++)
+            {
+                people[i].x = rawMessage.people[i].pos.x;
+                people[i].y = rawMessage.people[i].pos.y;
+                people[i].z = rawMessage.people[i].pos.z;
+            }
+
+        }
+    }
+}
