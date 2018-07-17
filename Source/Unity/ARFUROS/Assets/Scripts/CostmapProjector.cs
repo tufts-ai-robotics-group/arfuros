35648bc1fbfb11f476a23c5f20f02e1c812990b7
Amel <amelhassan98@yahoo.com>
2018-07-17 18:44:23 -0400

    Changed Costmap Coloring

--- Source/Unity/ARFUROS/Assets/Scripts/CostmapProjector.cs
+++ Source/Unity/ARFUROS/Assets/Scripts/CostmapProjector.cs
@@ -14,16 +14,12 @@ public class CostmapProjector : MonoBehaviour {
 	private const int UNKNOWN = -1;
 
 	// Color Variables 
-	Color lightGreen = new Color(0f,1f,0f,0.5f);
-	Color darkGreen = new Color(0f,0.5f,0f,0.5f);
-	Color greenYellow = new Color(0.7f,0.92f,0.016f,0.5f);
-	Color warmYellow = new Color(1f,0.92f,0.016f,0.5f);
-	Color yellowOrange = new Color(1f,0.85f,0.2f,0.5f);
-	Color standardOrange = new Color(1f,0.5f,0f,0.5f);
-	Color redOrange = new Color(1f,0.3f,0.05f,0.5f);
-	Color pinkRed = new Color(0.7f,0.2f,0.3f,0.5f);
-	Color trueRed = new Color(1f,0f,0f,0.5f);
-	Color darkRed = new Color(1f,0f,0f,0.5f);
+	float s_value = 0.84f;
+	float v_value = 1f;
+	float h_value;
+	float opacity = 0.7f;
+	const float Hue_Dividor = 3f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -70,8 +66,7 @@ public class CostmapProjector : MonoBehaviour {
         			}
         			if (message.ProbData[i] != UNKNOWN) // or maybe we should make it a diff color?
         			{
-            			particles[i].position = new Vector3(x_pos, 
-            				y_pos, 0f); 
+            			particles[i].position = new Vector3(x_pos, y_pos, 0f); 
         			 
             			ColorDecider(i);
             			particles[i].startSize = 0.025f;
@@ -88,25 +83,8 @@ public class CostmapProjector : MonoBehaviour {
 
 	void ColorDecider(int index){
 
-		if (message.ProbData[index] > -1 && message.ProbData[index] < 11)
-			particles[index].startColor = lightGreen;
-		else if (message.ProbData[index] > 10 && message.ProbData[index] < 21)
-			particles[index].startColor = darkGreen;
-		else if (message.ProbData[index] > 20 && message.ProbData[index] < 31)
-			particles[index].startColor = greenYellow;
-		else if (message.ProbData[index] > 30 && message.ProbData[index] < 41)
-			particles[index].startColor = warmYellow;
-		else if (message.ProbData[index] > 40 && message.ProbData[index] < 51)
-			particles[index].startColor = yellowOrange;
-		else if (message.ProbData[index] > 50 && message.ProbData[index] < 61)
-			particles[index].startColor = standardOrange;
-		else if (message.ProbData[index] > 60 && message.ProbData[index] < 71)
-			particles[index].startColor = redOrange;
-		else if (message.ProbData[index] > 70 && message.ProbData[index] < 81)
-			particles[index].startColor = pinkRed;
-		else if (message.ProbData[index] > 80 && message.ProbData[index] < 91)
-			particles[index].startColor = trueRed;
-		else
-			particles[index].startColor = darkRed;
+		h_value = ((1f - (message.ProbData[index]/100f)))/ Hue_Dividor;
+		particles[index].startColor = Color.HSVToRGB(h_value, s_value, v_value);
+		
 	}
 }
