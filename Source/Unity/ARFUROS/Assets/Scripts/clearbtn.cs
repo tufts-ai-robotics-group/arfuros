ad527c5034dbf9cff61c1b775ab6b4f0a8aaf342
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-07-18 15:01:07 -0400

    NumberPad Added

---
+++ Source/Unity/ARFUROS/Assets/Scripts/clearbtn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clearbtn : MonoBehaviour
+{
+
+    public InputField IPAddress;
+    public Button myButton;
+    private void Start()
+    {
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+    }
+    void Click()
+    {
+        IPAddress.text = "";
+    }
+}
---
+++ Source/Unity/ARFUROS/Assets/Scripts/deletebtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deletebtn : MonoBehaviour
+{
+
+    public InputField IPAddress;
+    public Button myButton;
+    private void Start()
+    {
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+    }
+    void Click()
+    {
+        if (IPAddress.text.Length > 1)
+            IPAddress.text = IPAddress.text.Substring(0, IPAddress.text.Length - 1);
+    }
+}
