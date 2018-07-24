e9a57970a96c9c7e9d3bbb98b7bd8f502b7ed0ae
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-07-24 15:52:15 -0400

    UI Panel Improved

---
+++ Source/Unity/ARFUROS/Assets/FaceCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera cameraToFollow;
+
+    void Update()
+    {
+        transform.LookAt(transform.position + cameraToFollow.transform.rotation * Vector3.forward,
+            cameraToFollow.transform.rotation * Vector3.up);
+    }
+}
