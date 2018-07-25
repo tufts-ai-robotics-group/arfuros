bd85c77575259b2c58121138bc4e896ee1fe0bb2
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-07-25 16:40:20 -0400

    Cursor Update

---
+++ Source/Unity/ARFUROS/Assets/Scripts/CursorProjector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorProjector : MonoBehaviour {
+    public GameObject cursor;
+    private RaycastHit hit;
+
+    int layermask = 1 << 5;
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layermask))
+        {
+            cursor.SetActive(true);
+            cursor.transform.position = hit.point;
+        }
+        else
+            cursor.SetActive(false);
+    }
+}
