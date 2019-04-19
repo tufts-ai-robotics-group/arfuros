7bb18aeb59e42c6e9db17057097ad2ef85128f38
Faizan Muhammad <faizan.muhammad@tufts.edu>
2019-04-19 13:58:34 -0400

    Blinkers Implemented

---
+++ Source/Unity/ARFUROS/Assets/BlinkerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+
+    public class BlinkerController : MonoBehaviour
+    {
+        public StringReceiver stringReceiver;
+        public SpriteBlinker leftBlinker;
+        public SpriteBlinker rightBlinker;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(stringReceiver.string_val == "R")
+            {
+                rightBlinker.isBlinking = true;
+                leftBlinker.isBlinking = false;
+            }
+            else if (stringReceiver.string_val == "L")
+            {
+                rightBlinker.isBlinking = false;
+                leftBlinker.isBlinking = true;
+            }
+            else
+            {
+                rightBlinker.isBlinking = false;
+                leftBlinker.isBlinking = false;
+            }
+        }
+    }
+}
---
+++ Source/Unity/ARFUROS/Assets/Scripts/CustomRosSharp/StringReceiver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class StringReceiver : MessageReceiver
+    {
+        public override Type MessageType { get { return (typeof(StandardString)); } }
+        private StandardString rawMessage;
+
+        public String string_val;
+
+        private void Awake()
+        {
+            MessageReception += ReceiveMessage;
+        }
+
+        private void ReceiveMessage(object sender, MessageEventArgs e)
+        {
+            rawMessage = (StandardString)e.Message;
+            UpdateValues();
+        }
+
+        private void UpdateValues()
+        {
+            string_val = rawMessage.data;
+        }
+    }
+}
